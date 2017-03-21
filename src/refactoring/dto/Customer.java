18c0dd87ac771f3c64208f89ad47e2fb0d0f28db
kdh82 <hwanta112@naver.com>
2017-03-21 10:53:22 +0900

    htmlStatment

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -26,6 +26,16 @@ public class Customer {
 		sb.append(String.format("누적 대여료: %s%n 적립포인트: %s%n", getTotalCharge(), getTotalFrequentRenterPoints()));
 		return sb.toString();
 	}
+	
+	public String htmlStatment(){
+		StringBuilder sb = new StringBuilder();
+		sb.append("<h1><em>"+getName()+"고객님의 대여 기록 </em></h1><p>\n"); //고객명
+		for(Rental each : rentals){		
+			sb.append(String.format("%s %s<br>%n", each.getMovie().getTitle(),each.getCharge()));		
+		}//for loop	
+		sb.append(String.format("<p>누적 대여료: <em>%s</em>%n<p> 적립포인트: <em>%s</em>%n", getTotalCharge(), getTotalFrequentRenterPoints()));
+		return sb.toString();
+	}
 
 	private Object getTotalFrequentRenterPoints() {
 		double result=0;
