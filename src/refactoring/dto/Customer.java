d2d03763c3ab79e4c4aaf0212dcc7c04221d243f
kdh82 <hwanta112@naver.com>
2017-03-21 10:40:29 +0900

    임시변수완료(getTotalcharge(), 갯토탈프리퀀시, uml완료

--- src/refactoring/dto/Customer.java
+++ src/refactoring/dto/Customer.java
@@ -7,36 +7,39 @@ public class Customer {
 	private String name;
 	private List<Rental> rentals;
 	
-	
 	public Customer(String name) {
 		this.name = name;
 		this.rentals = new ArrayList<>();
 	}
-
 	public String getName() {
 		return name;
-	}
-	
+	}	
 	public void addRental(Rental aRental){
-		this.rentals.add(aRental);
-		
-	}
-	
+		this.rentals.add(aRental);		
+	}	
 	public String statment(){
-		//대여료와 적립포인트 출력
-		double totalAmount=0; //대여료
-		int frequentRenterPoints =0; //적립포인트 지역변수는 클래스다이어그램안함
 		StringBuilder sb = new StringBuilder();
 		sb.append(getName()+"고객님의 대여기록 \n"); //고객명
-		for(Rental each : rentals){
-			frequentRenterPoints += each.getfrequentRenterPoints();
-			
-			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(),each.getCharge()));
-			totalAmount += each.getCharge();
-		}//for loop
-		
-		sb.append(String.format("누적 대여료: %s%n 적립포인트: %s%n", totalAmount, frequentRenterPoints));
-
+		for(Rental each : rentals){		
+			sb.append(String.format("\t%s \t %s%n", each.getMovie().getTitle(),each.getCharge()));		
+		}//for loop	
+		sb.append(String.format("누적 대여료: %s%n 적립포인트: %s%n", getTotalCharge(), getTotalFrequentRenterPoints()));
 		return sb.toString();
+	}
+
+	private Object getTotalFrequentRenterPoints() {
+		double result=0;
+		for(Rental each : rentals){
+			result += each.getfrequentRenterPoints();
+		}
+		return result;
+		}
+	private double getTotalCharge() {
+		double result=0.0;
+		for(Rental each : rentals){
+			result += each.getCharge();
+		}
+		return result;
+		}		
 	}	
-}
+
